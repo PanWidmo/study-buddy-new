c91292a04a3ca0aeb184e6e83d48c98ac9145592
PanWidmo <tomasz.baranww@gmail.com>
2021-11-07 18:13:33 +0100

    Finished styling UsersListItem component

--- src/components/atoms/Button/Button.styles.js
+++ src/components/atoms/Button/Button.styles.js
@@ -3,13 +3,14 @@ import styled from 'styled-components';
 export const StyledButton = styled.button`
   width: 25px;
   height: 25px;
-  background-color: ${({ theme }) => theme.colors.gray};
+  background-color: ${({ theme }) => theme.colors.grey};
   border-radius: 50px;
   border: none;
   color: white;
   display: flex;
   justify-content: center;
   align-items: center;
+  margin: 0 10px;
 
   svg {
     width: 100%;
--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -1,45 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '../../atoms/Button/Button';
-import { Wrapper } from './UsersListItem.styles';
-import styled from 'styled-components';
-
-const Avarage = styled.div`
-  background: ${({ theme, value }) => {
-    if (value > 4) return theme.colors.success;
-    if (value > 3) return theme.colors.warning;
-    if (value > 2) return theme.colors.error;
-    return theme.colors.grey;
-  }};
-  padding: 8px;
-  border-radius: 50%;
-  font-size: ${({ theme }) => theme.fontSize.m};
-  color: ${({ theme }) => theme.colors.white};
-  font-weight: bold;
-`;
-
-const NameAndAttendance = styled.div`
-  margin: 0 10px;
-`;
-
-const Name = styled.p`
-  font-weight: bold;
-  font-size: ${({ theme }) => theme.fontSize.l};
-`;
-
-const Attendance = styled.p`
-  font-size: ${({ theme }) => theme.fontSize.s};
-`;
+import { Wrapper, StyledAvarage, StyledNameAndAttendance } from './UsersListItem.styles';
 
 const UsersListItem = ({ userData: { average, name, attendance = '0%' } }) => {
   return (
     <Wrapper>
-      <Avarage value={average}>{average}</Avarage>
-      <NameAndAttendance>
-        <Name>{name}</Name>
-        <Attendance>attendance: {attendance}</Attendance>
-      </NameAndAttendance>
-      <Button />
+      <StyledAvarage value={average}>{average}</StyledAvarage>
+      <StyledNameAndAttendance>
+        <p>
+          {name}
+          <Button />
+        </p>
+        <p>attendance: {attendance}</p>
+      </StyledNameAndAttendance>
     </Wrapper>
   );
 };
