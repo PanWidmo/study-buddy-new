d03527cfc66a0d0e5288a08df99c6f890f69b784
PanWidmo <tomasz.baranww@gmail.com>
2021-11-20 13:57:54 +0100

    Class Component- toggle function with prevState

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -3,16 +3,28 @@ import { users } from 'data/users.js';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 import { Wrapper, StyledList } from './UsersList.styles';
 
-function UsersList() {
-  return (
-    <Wrapper>
-      <StyledList>
-        {users.map((userData, i) => (
-          <UsersListItem index={i} key={userData.name} userData={userData} />
-        ))}
-      </StyledList>
-    </Wrapper>
-  );
+class UsersList extends React.Component {
+  state = {
+    isUsersList: true,
+  };
+
+  toggleListTitle = () => {
+    this.setState((prevState) => ({ isUsersList: !prevState.isUsersList }));
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <h1>{this.state.isUsersList ? `User's list` : `Student's list`}</h1>
+        <button onClick={this.toggleListTitle}>Change title</button>
+        <StyledList>
+          {users.map((userData, i) => (
+            <UsersListItem index={i} key={userData.name} userData={userData} />
+          ))}
+        </StyledList>
+      </Wrapper>
+    );
+  }
 }
 
 export default UsersList;
