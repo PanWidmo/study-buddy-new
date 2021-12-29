21d6bf005d961aa576a6331102f6526449b5c82c
PanWidmo <tomasz.baranww@gmail.com>
2021-12-29 18:46:11 +0100

    Fixed styles problem

--- src/components/molecules/ViewWrapper/ViewWrapper.js
+++ src/components/molecules/ViewWrapper/ViewWrapper.js
@@ -2,9 +2,8 @@ import styled from 'styled-components';
 
 export const ViewWrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.white};
-  width: 100%;
   max-width: 500px;
-  padding: 25px 30px;
+  padding: 45px 70px;
   border-radius: 25px;
   box-shadow: 0 5px 15px -10px rgba(0, 0, 0, 0.3);
   margin: 25px;
