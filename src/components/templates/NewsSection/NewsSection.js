8c5f37f218386945b61581778079f58e226cd7e1
PanWidmo <tomasz.baranww@gmail.com>
2021-12-30 19:07:03 +0100

    Added special flag to Button (isBig)

--- src/components/templates/NewsSection/NewsSection.js
+++ src/components/templates/NewsSection/NewsSection.js
@@ -1,17 +1,23 @@
-import { ViewWrapper } from 'components/molecules/ViewWrapper/ViewWrapper';
 import React from 'react';
-import { Wrapper } from './NewsSection.styles';
+import { Wrapper, NewsSectionHeader, ArticleWrapper, TitleWrapper } from './NewsSection.styles';
 import { Button } from 'components/atoms/Button/Button';
 
 const NewsSection = () => {
   return (
     <Wrapper>
-      <h2>News feed section</h2>
-      <ViewWrapper>
-        <h3>Title</h3>
-        <p>description</p>
-        <Button>Click</Button>
-      </ViewWrapper>
+      <NewsSectionHeader>Uniwersity news feed</NewsSectionHeader>
+      <ArticleWrapper>
+        <TitleWrapper>
+          <h3>New computers for all lectures</h3>
+          <p>Staff news</p>
+        </TitleWrapper>
+        <p>
+          Amet, diam, viverra nec pretium in nunc a. Pellentesque venenatis fames molestie non. Nulla neque, a a id elementum pretium aliquam. In
+          turpis sem vestibulum ut in ut. Fringilla orci, condimentum tellus leo nunc, vitae eu. Diam euismod enim integer facilisi sed. Pretium
+          hendrerit quis egestas eget at magna ac commodo volutpat.
+        </p>
+        <Button isBig>Read more</Button>
+      </ArticleWrapper>
     </Wrapper>
   );
 };
--- src/components/templates/NewsSection/NewsSection.styles.js
+++ src/components/templates/NewsSection/NewsSection.styles.js
@@ -1,7 +1,37 @@
+import { ViewWrapper } from 'components/molecules/ViewWrapper/ViewWrapper';
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   grid-row: 1/3;
   grid-column: 3/3;
   border: 1px solid blue;
+  display: grid;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 50px;
+`;
+
+export const NewsSectionHeader = styled.h3`
+  margin-right: auto;
+  color: ${({ theme }) => theme.colors.darkGrey};
+`;
+
+export const ArticleWrapper = styled(ViewWrapper)`
+  margin: 0;
+  width: 100%;
+  max-width: unset;
+  border-radius: 10px;
+  color: ${({ theme }) => theme.colors.darkGrey};
+`;
+
+export const TitleWrapper = styled.div`
+  margin: 0;
+  h3 {
+    font-size: ${({ theme }) => theme.fontSize.xl};
+  }
+
+  p {
+    font-size: ${({ theme }) => theme.fontSize.m};
+  }
 `;
