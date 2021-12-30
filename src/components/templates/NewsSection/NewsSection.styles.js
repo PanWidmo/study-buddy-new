95a483ffc4a9d3f4f753c1010d06e5c057d05414
PanWidmo <tomasz.baranww@gmail.com>
2021-12-30 19:20:13 +0100

    Styling single article

--- src/components/templates/NewsSection/NewsSection.styles.js
+++ src/components/templates/NewsSection/NewsSection.styles.js
@@ -5,11 +5,12 @@ export const Wrapper = styled.div`
   grid-row: 1/3;
   grid-column: 3/3;
   border: 1px solid blue;
-  display: grid;
+  display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
   padding: 50px;
+  overflow-y: scroll;
 `;
 
 export const NewsSectionHeader = styled.h3`
@@ -19,19 +20,25 @@ export const NewsSectionHeader = styled.h3`
 
 export const ArticleWrapper = styled(ViewWrapper)`
   margin: 0;
+  padding: 25px;
   width: 100%;
   max-width: unset;
   border-radius: 10px;
   color: ${({ theme }) => theme.colors.darkGrey};
+
+  p {
+    line-height: 1.6;
+  }
 `;
 
 export const TitleWrapper = styled.div`
-  margin: 0;
   h3 {
     font-size: ${({ theme }) => theme.fontSize.xl};
+    margin: 0;
   }
 
   p {
     font-size: ${({ theme }) => theme.fontSize.m};
+    margin: 0;
   }
 `;
