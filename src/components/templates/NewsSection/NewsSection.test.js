3359350a8cd548515759c5c2d5e0902a9e585f1b
PanWidmo <tomasz.baranww@gmail.com>
2021-12-31 09:39:32 +0100

    Fixed AddUser.test and started NewsSection.test

---
+++ src/components/templates/NewsSection/NewsSection.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+describe('News Section', () => {
+  it('Render articles', () => {
+    console.log('Test');
+  });
+});
--- src/views/AddUser.test.js
+++ src/views/AddUser.test.js
@@ -5,7 +5,7 @@ import { screen, fireEvent } from '@testing-library/react';
 import { renderWithProviders } from 'helpers/renderWithProviders';
 
 describe('Add User', () => {
-  it('Renders the component', () => {
+  it('Adds new user to the list', () => {
     renderWithProviders(
       <>
         <AddUser />
@@ -15,6 +15,7 @@ describe('Add User', () => {
     fireEvent.change(screen.getByLabelText('Name'), { target: { value: 'Grażyna' } });
     fireEvent.change(screen.getByLabelText('Attendance'), { target: { value: '55%' } });
     fireEvent.change(screen.getByLabelText('Average'), { target: { value: '4.5' } });
+    fireEvent.click(screen.getByLabelText('Consent'));
     fireEvent.click(screen.getByText('Add'));
     screen.getByText('Grażyna');
   });
