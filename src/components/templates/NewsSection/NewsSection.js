13a09b604df2d5b57af63e3b7a39cc82a3da4c1e
PanWidmo <tomasz.baranww@gmail.com>
2021-12-31 09:31:17 +0100

    Install axios, fetching news from DatoCMS

--- src/components/templates/NewsSection/NewsSection.js
+++ src/components/templates/NewsSection/NewsSection.js
@@ -1,46 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Wrapper, NewsSectionHeader, ArticleWrapper, TitleWrapper, ContentWrapper } from './NewsSection.styles';
 import { Button } from 'components/atoms/Button/Button';
+import axios from 'axios';
 
-const data = [
-  {
-    title: 'New computers at school',
-    category: 'Tech news',
-    content:
-      'Amet, diam, viverra nec pretium in nunc a. Amet, diam, viverra nec pretium in nunc a.Amet, diam, viverra nec pretium in nunc a.Amet, diam, viverra nec pretium in nunc a.Amet, diam, viverra nec pretium in nunc a.',
-  },
-  {
-    title: 'New computers at school1',
-    category: 'Tech news',
-    content:
-      'Amet, diam, viverra nec pretium in nunc a. Amet, diam, viverra nec pretium in nunc a.Amet, diam, viverra nec pretium in nunc a.Amet, diam, viverra nec pretium in nunc a.Amet, diam, viverra nec pretium in nunc a.',
-    image: 'https://unsplash.it/500/400',
-  },
-  {
-    title: 'New computers at school2',
-    category: 'Tech news',
-    content:
-      'Amet, diam, viverra nec pretium in nunc a. Amet, diam, viverra nec pretium in nunc a.Amet, diam, viverra nec pretium in nunc a.Amet, diam, viverra nec pretium in nunc a.Amet, diam, viverra nec pretium in nunc a.',
-  },
-];
+export const query = `
+         {
+          allArticles {
+            id
+            title
+            category
+            content
+            image {
+              url
+            }
+          }
+        }
+      `;
 
 const NewsSection = () => {
+  const [articles, setArticles] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    console.log(process.env.REACT_APP_DATOCMS_TOKEN);
+    axios
+      .post(
+        'https://graphql.datocms.com/',
+        {
+          query,
+        },
+        {
+          headers: {
+            authorization: `Bearer ' + ${process.env.REACT_APP_DATOCMS_TOKEN}`,
+          },
+        }
+      )
+      .then(({ data: { data } }) => {
+        setArticles(data.allArticles);
+      })
+      .catch(() => {
+        setError(`Sorry, we couldn't load articles for you`);
+      });
+  }, []);
+
   return (
     <Wrapper>
       <NewsSectionHeader>University news feed</NewsSectionHeader>
-      {data.map(({ title, category, content, image = null }) => (
-        <ArticleWrapper key={title}>
-          <TitleWrapper>
-            <h3>{title}</h3>
-            <p>{category}</p>
-          </TitleWrapper>
-          <ContentWrapper>
-            <p>{content}</p>
-            {image ? <img src={image} alt="article img" /> : null}
-          </ContentWrapper>
-          <Button isBig>Read more</Button>
-        </ArticleWrapper>
-      ))}
+      {articles.length > 0 ? (
+        articles.map(({ id, title, category, content, image = null }) => (
+          <ArticleWrapper key={id}>
+            <TitleWrapper>
+              <h3>{title}</h3>
+              <p>{category}</p>
+            </TitleWrapper>
+            <ContentWrapper>
+              <p>{content}</p>
+              {image ? <img src={image.url} alt="article" /> : null}
+            </ContentWrapper>
+            <Button isBig>Read more</Button>
+          </ArticleWrapper>
+        ))
+      ) : (
+        <NewsSectionHeader>{error ? error : 'Loading...'}</NewsSectionHeader>
+      )}
     </Wrapper>
   );
 };
