17d9c0dd28ce5939a3fd9162dc69ba7587a02718
PanWidmo <tomasz.baranww@gmail.com>
2022-01-27 20:51:14 +0100

    Created story for StudentsListItem, changes in styles of it

--- src/components/molecules/StudentsListItem/StudentsListItem.styles.js
+++ src/components/molecules/StudentsListItem/StudentsListItem.styles.js
@@ -19,7 +19,7 @@ export const StyledAvarage = styled.div`
   background: ${({ theme, value }) => {
     if (value > 4) return theme.colors.success;
     if (value > 3) return theme.colors.warning;
-    if (value > 2) return theme.colors.error;
+    if (value > 1) return theme.colors.error;
     return theme.colors.grey;
   }};
   display: flex;
@@ -30,6 +30,8 @@ export const StyledAvarage = styled.div`
   font-size: ${({ theme }) => theme.fontSize.m};
   color: ${({ theme }) => theme.colors.white};
   font-weight: bold;
+  width: 35px;
+  height: 35px;
 `;
 
 export const StyledNameAndAttendance = styled.div`
